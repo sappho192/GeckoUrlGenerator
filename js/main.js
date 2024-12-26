8553c8d63b9630d9c6385b506b223aa89736b490
Taein KIM <sappho192@gmail.com>
2024-12-26 18:06:09 +0900

    Properly add transcript urls

--- js/main.js
+++ js/main.js
@@ -39,10 +39,33 @@ generateURL = () => {
     let url = new String(baseURL);
     url += "/?save_mode=server";
     url += `&audio=${audioURL}`;
+
+    const rttmList = [];
+    const jsonList = [];
     // Check transcript type
     for (let i = 0; i < transcriptURLs.length; i++) {
       const transcriptType = document.querySelector(`#transcript-type-select-${i}`).value;
-      url += `&${transcriptType}=${transcriptURLs[i]}`;
+      if (transcriptType === "rttm") {
+        rttmList.push(transcriptURLs[i]);
+      } else if (transcriptType === "json") {
+        jsonList.push(transcriptURLs[i]);
+      }
+    }
+
+    for (let i = 0; i < rttmList.length; i++) {
+      if (i === 0) {
+        url += `&rttm=${rttmList[i]}`;
+      } else {
+        url += `;${rttmList[i]}`;
+      }
+    }
+
+    for (let i = 0; i < jsonList.length; i++) {
+      if (i === 0) {
+        url += `&json=${jsonList[i]}`;
+      } else {
+        url += `;${jsonList[i]}`;
+      }
     }
 
     console.log(url);
