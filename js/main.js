61533b80f86ff91785f1573772fd45a8a7b4983f
Taein KIM <sappho192@gmail.com>
2024-12-26 17:44:54 +0900

    Set transcript type respectively

--- js/main.js
+++ js/main.js
@@ -1,23 +1,47 @@
+var inputIndex = 1;
+
 addTranscriptInput = () => {
+    const transcriptDiv = document.createElement("div");
+    transcriptDiv.id = `transcript-div-${inputIndex}`;
+
     const transcriptLabel = document.createElement("label");
     transcriptLabel.innerText = "Transcript URL: ";
     const transcriptInput = document.createElement("input");
     transcriptInput.type = "text";
+    transcriptInput.id = `transcript-input-${inputIndex}`;
     transcriptLabel.appendChild(transcriptInput);
-    document.querySelector("#transcript-inputs").appendChild(transcriptLabel);
+    transcriptDiv.appendChild(transcriptLabel);
+
+    const transcriptTypeSelect = document.createElement("select");
+    transcriptTypeSelect.id = `transcript-type-select-${inputIndex}`;
+    const rttmOption = document.createElement("option");
+    rttmOption.value = "rttm";
+    rttmOption.innerText = "RTTM";
+    const jsonOption = document.createElement("option");
+    jsonOption.value = "json";
+    jsonOption.innerText = "JSON";
+    transcriptTypeSelect.appendChild(rttmOption);
+    transcriptTypeSelect.appendChild(jsonOption);
+    transcriptDiv.appendChild(transcriptTypeSelect);
+
+    document.querySelector("#transcript-inputs").appendChild(transcriptDiv);
+    inputIndex++;
 }
 
 generateURL = () => {
     const baseURL = document.querySelector("#input-base-url").value;
     const audioURL = document.querySelector("#input-audio-url").value;
-    const transcriptURLs = Array.from(document.querySelectorAll("#transcript-inputs input")).map(input => input.value);
+    const transcriptURLs = [];
+    for (let i = 0; i < inputIndex; i++) {
+      transcriptURLs.push(document.querySelector(`#transcript-input-${i}`).value);
+    }
   
     let url = new String(baseURL);
     url += "/?save_mode=server";
     url += `&audio=${audioURL}`;
     // Check transcript type
-    const transcriptType = document.querySelector("#transcript-type-select").value;
     for (let i = 0; i < transcriptURLs.length; i++) {
+      const transcriptType = document.querySelector(`#transcript-type-select-${i}`).value;
       url += `&${transcriptType}=${transcriptURLs[i]}`;
     }
 
